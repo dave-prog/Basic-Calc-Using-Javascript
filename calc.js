9bb25675d8423cdff4c57eb07dac4edac77e173b
David <davidolanrewaju538@gmail.com>
2022-06-23 09:10:33 +0100

    Basic Javascript Calculator without frontend

---
+++ calc.js
@@ -0,0 +1,21 @@
+var a = prompt("Please enter a number");
+var b = prompt("Please enter another number");
+
+var a = parseInt(a);
+var b = parseInt(b);
+
+let addition = a + b;
+let subtraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+
+alert(
+	"Addition = " +
+	addition +
+	"\nSubtraction = " +
+	subtraction +
+	"\nMultiplication = " +
+	multiplication +
+	"\nDivision = " +
+	division
+);
